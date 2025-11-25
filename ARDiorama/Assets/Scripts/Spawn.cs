49b4a9c072a8c470b6555da711ca1ceb6befb2bc
Vladislav <pvvvlad2007@mail.ru>
2025-11-25 15:18:23 +0300

    0.1

--- ARDiorama/Assets/Scripts/Spawn.cs
+++ ARDiorama/Assets/Scripts/Spawn.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    public GameObject pref;
+    public GameObject Earth;
+    public GameObject House1;
 
+    private Camera cam;
 
     void Start()
     {
+        cam = Camera.main;
         EarthSpawn();
     }
 
-    private void EarthSpawn()
+	private void Update()
+	{
+		
+	}
+
+	private void EarthSpawn()
     {
-        for (float i = -4.5f; i < i+10; i++)
+        float firstI = -4.5f;
+        float firstY = -4.5f;
+
+        for (float i = firstI; i < firstI + 10; i++)
         {
-            for (float j = -4.5f;  j < j+10; j++)
+            for (float j = firstY; j < firstY + 10; j++)
             {
-                Instantiate(pref, new Vector3(i,0,j), Quaternion.identity);
+                Instantiate(Earth, new Vector3(i, 0, j), Quaternion.identity);
             }
         }
+    }
+
+    private void SpawnRay()
+    {
+        
     }
 }
